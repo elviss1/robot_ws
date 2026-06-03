5b30bd0dcc5762591502692fad5c064d1554a051
Ubunturse <12345678+username@users.noreply.github.com>
2026-06-02 22:44:32 -0600

    Day 4: Add obstacle stop behavior

--- src/lidar_reader/lidar_reader/lidar_reader.py
+++ src/lidar_reader/lidar_reader/lidar_reader.py
@@ -2,6 +2,7 @@ import math
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import TwistStamped                                                                                                                                                                                                               
 
 
 class LidarReader(Node):
@@ -15,6 +16,11 @@ class LidarReader(Node):
             self.scan_callback,
             10
         )
+        self.publisher = self.create_publisher(
+            TwistStamped, 
+            '/cmd_vel', 
+            10    
+        )
 
     def scan_callback(self, msg):
 
@@ -22,7 +28,7 @@ class LidarReader(Node):
             r for r in msg.ranges
             if r > 0.0 and math.isfinite(r)
         ]
-
+                                                                                                        
         if len(valid_ranges) > 0:
             nearest = min(valid_ranges)
             nearest_index = msg.ranges.index(nearest)
@@ -31,6 +37,18 @@ class LidarReader(Node):
                 msg.angle_min
                 + nearest_index * msg.angle_increment
             )
+            if nearest < 0.75:
+
+                stop_msg = TwistStamped()
+
+                stop_msg.twist.linear.x = 0.0
+                stop_msg.twist.angular.z = 0.0
+
+                self.publisher.publish(stop_msg)
+
+                self.get_logger().info(
+                    'Obstacle detected! STOP command sent'
+                )
 
             self.get_logger().info(
                 f'Nearest obstacle: {nearest:.2f} m, '
