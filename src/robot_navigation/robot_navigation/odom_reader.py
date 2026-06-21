db0728d33d564dd941c5e98ce996e368ac4f639a
Ubunturse <12345678+username@users.noreply.github.com>
2026-06-21 10:29:01 -0600

    Day 15: Add pose tracking with yaw extraction

--- src/robot_navigation/robot_navigation/odom_reader.py
+++ src/robot_navigation/robot_navigation/odom_reader.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from nav_msgs.msg import Odometry
+import math
 
 class OdomReader(Node):
     def __init__(self):
@@ -16,14 +17,32 @@ class OdomReader(Node):
         x = msg.pose.pose.position.x
         y = msg.pose.pose.position.y
 
+        qx = msg.pose.pose.orientation.x
+        qy = msg.pose.pose.orientation.y
+        qz = msg.pose.pose.orientation.z
+        qw = msg.pose.pose.orientation.w
+
         linear_x = msg.twist.twist.linear.x
         angular_z = msg.twist.twist.angular.z  
+
+        yaw = math.atan2(
+            2.0 * (qw * qz + qx * qy),
+            1.0 - 2.0 * (qy * qy + qz * qz)
+        )
+        
         self.get_logger().info(
-            f'x: {x: .2f}, '
-            f'y: {y: .2f}, '
+            f'x: {x:.2f}, '
+            f'y: {y:.2f}, '
+            f'yaw: {yaw:.2f} rad'
             f'linear_x: {linear_x: .2f}, '
             f'angular_z: {angular_z: .2f}, '
         )
+        # self.get_logger().info(
+        #     f'x: {x: .2f}, '
+        #     f'y: {y: .2f}, '
+        #     f'linear_x: {linear_x: .2f}, '
+        #     f'angular_z: {angular_z: .2f}, '
+        # )
 
        # 
 def main(args=None):
