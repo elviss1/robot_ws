c59eefe4214e45b7691419f3465ea63725479d4b
Ubunturse <12345678+username@users.noreply.github.com>
2026-07-08 07:42:10 -0600

    Day 23: Refactor wapoint driver into finite state machine

--- src/robot_navigation/robot_navigation/waypoint_driver.py
+++ src/robot_navigation/robot_navigation/waypoint_driver.py
@@ -17,7 +17,9 @@ class WaypointDriver(Node):
         self.heading_tolerance = 0.1
         self.closest_obstacle_distance = float("inf")
         self.safety_distance = 0.5
+        self.state = "ROTATING"
 
+        
         self.scan_subscription = self.create_subscription(
             LaserScan,
             '/scan',
@@ -88,23 +90,41 @@ class WaypointDriver(Node):
         heading_error = self.normalize_angle(desired_heading - current_yaw)
 
         cmd_msg = TwistStamped()
-        status = "unknown"
+
+        # -------------------------
+        # Phase 1: Choose state
+        # -------------------------
 
         if distance_to_goal <= self.distance_tolerance:
-            if self.current_waypoints == 2:
-                cmd_msg.twist.linear.x = 0.0
-                cmd_msg.twist.angular.z = 0.0
-                status = "stopped"
+            if self.current_waypoints == len(self.waypoints) - 1:
+                self.state = "MISSION_COMPLETE"
             else:
                 self.current_waypoints += 1
-                status = "next_waypoint"
+                self.state = "ROTATING"
 
         elif self.closest_obstacle_distance < self.safety_distance:
+            self.state = "BLOCKED"
+
+        elif abs(heading_error) > self.heading_tolerance:
+            self.state = "ROTATING"
+
+        else:
+            self.state = "DRIVING"
+
+
+        # -------------------------
+        # Phase 2: Execute state
+        # -------------------------
+
+        if self.state == "MISSION_COMPLETE":
             cmd_msg.twist.linear.x = 0.0
             cmd_msg.twist.angular.z = 0.0
-            status = "blocked_by_obstacle"
-    
-        elif abs(heading_error) > self.heading_tolerance:
+
+        elif self.state == "BLOCKED":
+            cmd_msg.twist.linear.x = 0.0
+            cmd_msg.twist.angular.z = 0.0
+
+        elif self.state == "ROTATING":
             cmd_msg.twist.linear.x = 0.0
 
             if heading_error > 0:
@@ -112,13 +132,41 @@ class WaypointDriver(Node):
             else:
                 cmd_msg.twist.angular.z = -0.4
 
-            status = "rotating"
-
-        else:
+        elif self.state == "DRIVING":
             gain = 0.5
             cmd_msg.twist.linear.x = 0.2
             cmd_msg.twist.angular.z = gain * heading_error
-            status = "driving_correcting"
+        # status = "unknown"
+
+        # if distance_to_goal <= self.distance_tolerance:
+        #     if self.current_waypoints == 2:
+        #         cmd_msg.twist.linear.x = 0.0
+        #         cmd_msg.twist.angular.z = 0.0
+        #         status = "stopped"
+        #     else:
+        #         self.current_waypoints += 1
+        #         status = "next_waypoint"
+
+        # elif self.closest_obstacle_distance < self.safety_distance:
+        #     cmd_msg.twist.linear.x = 0.0
+        #     cmd_msg.twist.angular.z = 0.0
+        #     status = "blocked_by_obstacle"
+    
+        # elif abs(heading_error) > self.heading_tolerance:
+        #     cmd_msg.twist.linear.x = 0.0
+
+        #     if heading_error > 0:
+        #         cmd_msg.twist.angular.z = 0.4
+        #     else:
+        #         cmd_msg.twist.angular.z = -0.4
+
+        #     status = "rotating"
+
+        # else:
+        #     gain = 0.5
+        #     cmd_msg.twist.linear.x = 0.2
+        #     cmd_msg.twist.angular.z = gain * heading_error
+        #     status = "driving_correcting"
 
         self.publisher.publish(cmd_msg)
 
@@ -129,7 +177,7 @@ class WaypointDriver(Node):
             f'yaw: {current_yaw:.2f}, '
             f'desired: {desired_heading:.2f}, '
             f'error: {heading_error:.2f}, '
-            f'status: {status}'
+            f'state: {self.state}, '
             f'obstacle: {self.closest_obstacle_distance:.2f}, '
         )
 
